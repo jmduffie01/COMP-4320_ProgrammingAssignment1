64858ff62cc284d235987e040b73ff57f5b5983b
Joel Duffie <jmduffie01@gmail.com>
2022-09-12 11:03:35 -0500

    cleaning up

--- TCPEchoClient.java
+++ TCPEchoClient.java
@@ -7,11 +7,14 @@ public class TCPEchoClient {
 
     if ((args.length < 2) || (args.length > 3))  // Test for correct # of args
       throw new IllegalArgumentException("Parameter(s): <Server> <Word> [<Port>]");
+    
+    // Server name or IP address
+    String server = args[0];
 
-    String server = args[0];       // Server name or IP address
     // Convert input String to bytes using the default character encoding
     byte[] byteBuffer = args[1].getBytes();
 
+    // Check for port parameter, and set port; if no port, set as 7
     int servPort = (args.length == 3) ? Integer.parseInt(args[2]) : 7;
 
     // Create socket that is connected to server on specified port
@@ -21,20 +24,29 @@ public class TCPEchoClient {
     InputStream in = socket.getInputStream();
     OutputStream out = socket.getOutputStream();
 
-    out.write(byteBuffer);  // Send the encoded string to the server
+    // Send the encoded string to the server
+    out.write(byteBuffer);  
 
     // Receive the same string back from the server
-    int totalBytesRcvd = 0;  // Total bytes received so far
-    int bytesRcvd;           // Bytes received in last read
+
+    // Total bytes received so far
+    int totalBytesRcvd = 0;
+    // Bytes received in last read
+    int bytesRcvd;
+
+    // while the amt of rcvd bytes doesn't exceed the buffer length
     while (totalBytesRcvd < byteBuffer.length) {
+      // read data to byte buffer, until end of buffer reached
       if ((bytesRcvd = in.read(byteBuffer, totalBytesRcvd,  
                         byteBuffer.length - totalBytesRcvd)) == -1)
         throw new SocketException("Connection close prematurely");
+      // add bytes received in last read to total
       totalBytesRcvd += bytesRcvd;
     }
 
     System.out.println("Received: " + new String(byteBuffer));
 
-    socket.close();  // Close the socket and its streams
+    // Close the socket and its streams
+    socket.close();
   }
 }
